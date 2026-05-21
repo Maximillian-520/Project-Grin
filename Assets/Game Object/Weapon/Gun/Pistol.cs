28ab9b24f020257347e71067836035c274626708
Maximillian-520 <maximilliankenas520@gmail.com>
2026-05-21 11:59:10 +0700

    Fix gun problems and added extra effects

---
+++ Assets/Game Object/Weapon/Gun/Pistol.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Pistol : BaseWeapon
+{
+    public override void Trigger()
+    {
+        Debug.Log("Pew");
+    }
+}
--- Assets/Game Object/Weapon/Gun/Rifle.cs
+++ Assets/Game Object/Weapon/Gun/Rifle.cs
@@ -4,9 +4,17 @@ public class Rifle : BaseWeapon
 {
     [Header("Component and Object")]
     [SerializeField] private Camera fpsCamera;
+    [SerializeField] private Transform muzzlePosition;
+
     [Header("Gun Data")]
-    [SerializeField] private GameObject bulletHitEffect;
+    [SerializeField] private int fireRate = 6;
+    [SerializeField] private float maxSpread = 9f;
+    [SerializeField] private GameObject muzzleFlashEffectPrefab;
+    [SerializeField] private GameObject bulletHitEffectPrefab;
+    [SerializeField] private float muzzleFlashEffectDuration = 0.5f;
     [SerializeField] private float bulletHitEffectDuration = 0.5f;
+
+    private float nextFireTime = 0;
 
     // ====================================================================================================
     //                     Virtual Functions
@@ -16,20 +24,52 @@ public class Rifle : BaseWeapon
     {
         // Assertion check
         Debug.Assert(fpsCamera, "fpsCamera is missing");
-        Debug.Assert(bulletHitEffect, "bulletHitEffect is empty");
+        Debug.Assert(muzzlePosition, "muzzlePosition is missing");
+        Debug.Assert(muzzleFlashEffectPrefab, "muzzleFlashEffectPrefab is missing");
+        Debug.Assert(bulletHitEffectPrefab, "bulletHitEffectPrefab is empty");
     }
     #endregion
 
+    // ====================================================================================================
+    //                     Weapon Functions
+    // ====================================================================================================
+    #region Weapon
     public override void Trigger()
     {
+        // Check can fire
+        if (Time.time < nextFireTime) return;
+        // Fire
         RaycastHit hit;
-        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit))
+        bool isHit = Physics.Raycast(
+            fpsCamera.transform.position,
+            fpsCamera.transform.forward + GetSpreadOffset(fpsCamera.transform),
+            out hit
+        );
+        if (isHit)
         {
-            Debug.Log(hit.transform.name);
+
+            // Debug.Log(hit.transform.name);
+            
+            // Spawn bullet hit effect
             GameObject bulletHitEffectInstance = Instantiate(
-                bulletHitEffect, hit.point, Quaternion.LookRotation(hit.normal)
+                bulletHitEffectPrefab, hit.point, Quaternion.LookRotation(hit.normal)
             );
             Destroy(bulletHitEffectInstance, bulletHitEffectDuration);
         }
+        // Spawn muzzle flash effect
+        GameObject muzzleFlashEffectInstance = Instantiate(muzzleFlashEffectPrefab, muzzlePosition);
+        Destroy(muzzleFlashEffectInstance, muzzleFlashEffectDuration);
+        // Set fire time
+        nextFireTime = Time.time + (1.0f / (float)fireRate);
     }
+
+    private Vector3 GetSpreadOffset(Transform fpsTransform)
+    {
+        float offsetMagnitude = Random.Range(0f, maxSpread * Mathf.Deg2Rad);
+        Vector2 spreadDirection = Random.insideUnitCircle.normalized;
+        Vector3 offsetDirectionX = fpsTransform.right * spreadDirection.x * offsetMagnitude;
+        Vector3 offsetDirectionY = fpsTransform.up * spreadDirection.y * offsetMagnitude;
+        return offsetDirectionX + offsetDirectionY;
+    }
+    #endregion
 }
